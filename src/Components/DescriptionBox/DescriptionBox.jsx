55fd07b5aee6c35aac754a09ed20723f184608ef
ssocSpiros <theodossiouspiros@yahoo.gr>
2023-12-20 13:35:28 +0200

    ecommercedemo

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+
+ const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor, sit , aut placeat dicta sapiente voluptates voluptatem, quae quam eum illum magni iusto officia id rem? Amet, voluptatibus magni libero ea nihil quo expedita molestiae adipisci quas?</p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur reprehenderit ipsum illo natus reiciendis quam pariatur amet velit laborum repellat et modi, aliquid magni sunt. Esse maxime odio sapiente quibusdam.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+
+export default DescriptionBox
